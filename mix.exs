77d8052f50847c322ce56b586f157668a6c32331
Michael Truog <mjtruog@protonmail.com>
2020-11-26 14:22:26 -0800

    Version 2.0.1.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule SyslogSocket.Mixfile do
 
   def project do
     [app: :syslog_socket,
-     version: "2.0.0",
+     version: "2.0.1",
      language: :erlang,
      erlc_options: [
        {:d, :erlang.list_to_atom('ERLANG_OTP_VERSION_' ++ :erlang.system_info(:otp_release))},
